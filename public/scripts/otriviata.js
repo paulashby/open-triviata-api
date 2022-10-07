a634b8ad1158b530c5ffd36715fdf66ed56e5c1b
Paul Ashby <paul@primitive.co>
2022-10-07 11:59:17 +0100

    Show copy feedback for 1 second only

--- public/scripts/otriviata.js
+++ public/scripts/otriviata.js
@@ -5,10 +5,14 @@ $( document ).ready(function() {
 		// Doesn't work on localhost https://developer.mozilla.org/en-US/docs/Web/API/Clipboard
 		navigator.clipboard.writeText($('#generated-url').val().trim()).then(
 			() => {
-				$('#ot-clipboard').addClass('success');
+				$('#ot-clipboard').addClass('success').delay(1000).queue(function(){
+				    $(this).removeClass("success").dequeue();
+				});
 			},
 			() => {
-				$('#ot-clipboard').addClass('fail');
+				$('#ot-clipboard').addClass("fail").delay(10000).queue(function(){
+				    $(this).removeClass("fail").dequeue();
+				});
 			}
 		);
 	});
